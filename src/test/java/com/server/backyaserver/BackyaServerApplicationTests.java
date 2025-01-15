b9eb3c6b0bb66f73f070cba5ad21034c8d490698
박세준 <74056843+sejoon00@users.noreply.github.com>
2025-01-15 15:20:05 +0900

    [chore] 프로젝트 초기 설정

---
+++ src/test/java/com/server/backyaserver/BackyaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.backyaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackyaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
