751d3fe7047c36195efac9c19cda9c87d5a87435
ralph <sanghwaj222@gmail.com>
2025-01-15 22:14:47 +0900

    11

---
+++ src/main/java/com/server/backyaserver/BackyaServerApplication.java
@@ -0,0 +1,13 @@
+package com.server.backyaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackyaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackyaServerApplication.class, args);
+    }
+
+}
